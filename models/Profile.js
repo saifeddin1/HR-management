7e54141dbb2e073c954769228c4b6bb194f46d14
saifeddin1 <saifeddinmatoui4@gmail.com>
2022-01-26 11:20:13 +0100

    Add collaborators to profileSchema

--- models/Profile.js
+++ models/Profile.js
@@ -3,12 +3,23 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const CollaboratorSchema = Schema(
+    {
+        position: String,
+        departement: String,
+        proEmail: { type: String, unique: true },
+        image: String,
+    },
+    { timestamps: true }
+)
+
 const profileSchema = Schema(
     {
         position: String,
         departement: String,
         proEmail: { type: String, unique: true },
-        image: String
+        image: String,
+        collaborators: [{ type: CollaboratorSchema, default: new Object() }]
     },
     { timestamps: true }
 );
