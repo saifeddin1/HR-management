957f483c12dc50de89e847e986cd076edd892a79
saifeddin1 <saifeddinmatoui4@gmail.com>
2022-02-02 17:39:06 +0100

    Fix jwt verification

--- config/auth.js
+++ config/auth.js
@@ -4,9 +4,21 @@ const dotenv = require('dotenv');
 dotenv.config({ path: '.env' });
 
 
+// module.exports.auth = (req, res, next) => {
+
+//     const decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET, { algorithms: ["HS256"] })
+//     req.user = decoded;
+//     jwt.verify(req.headers.authorization, process.env.JWT_SECRET, { algorithms: ['HS256'] })
+//     next();
+// }
+
+
 module.exports.auth = (req, res, next) => {
-    const decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET, { algorithms: ['HS256'] })
+    // const accessToken = req.headers["x-access-token"];
+    const accessToken = req.headers?.authorization?.split(" ")[1];
+    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET)
     req.user = decoded;
-    jwt.verify(req.headers.authorization, process.env.JWT_SECRET, { algorithms: ['HS256'] })
+    console.log(req?.user);
+    jwt.verify(accessToken, process.env.JWT_SECRET)
     next();
 }
