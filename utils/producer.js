02f6d268c65dda346aa37033ab33be64ded2e562
Tarek chafroud <chafroudtarek6@gmail.com>
2022-05-03 19:46:26 +0100

    update kafka

---
+++ utils/producer.js
@@ -0,0 +1,40 @@
+
+const { Kafka } = require('kafkajs');
+
+
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092","localhost:9093"],
+});
+
+const producer = kafka.producer();
+
+ exports.updateUser = async (res) => {
+
+  
+  // Producing
+   console.log("update file ******",res)
+   try{
+
+   
+    await producer.connect();
+     await producer.send({
+      topic: `my-topic7`,
+      messages:[{ value: JSON.stringify(res)}] ,
+     
+
+    });
+    console.log("producer is working")
+  }catch(e){
+    console.log("error", e)
+  }  
+
+  }
+  
+  
+
+
+
+
+
